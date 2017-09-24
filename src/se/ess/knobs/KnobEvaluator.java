1e0393f3ed31443ca3ddc9bf93f135fa954298ba
Claudio Rosati <Claudio.Rosati@esss.se>
2017-09-24 16:05:47 +0200

    KNOBS 1.0.7 Converted from Maven-based project to Ant-based one.

--- src/se/ess/knobs/KnobEvaluator.java
+++ src/se/ess/knobs/KnobEvaluator.java
@@ -26,6 +26,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
 
 /**
  * @author Claudio Rosati, European Spallation Source ERIC
